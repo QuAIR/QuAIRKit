08794e777dc942798ac537c77cbdc286fa484c40
Tengxiang Lin <eeltx@icloud.com>
2025-05-28 15:02:09 +0800

    Revert "Update dependency installation and remove obsolete version handling in version.sh"
    This reverts commit 1075f8baf2fea71f01fb840e747e1a1d3727e093.

--- version.sh
+++ version.sh
@@ -61,6 +61,12 @@ for tag in "${tags[@]}"; do
             sphinx-build docs/sphinx_src docs/api/$tag
             rm -rf docs/sphinx_src
             ;;
+        v0.3.0 | *)
+            python docs/update_quairkit_rst.py
+            cp -r tutorials docs/sphinx_src/
+            sphinx-build docs/sphinx_src docs/api/$tag
+            rm -rf docs/sphinx_src
+            ;;
     esac
 done
 
